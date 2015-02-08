72947a243d089631494d18870afc67747d4681e1
maralkan <marveen1520@gmail.com>
2015-02-08 00:05:36 -0800

    Changes to contact.php
    Added php code for sending email;
    changed typo that caused errors

--- contact.php
+++ contact.php
@@ -1,7 +1,30 @@
+<?php
+if($_POST['submit']) {
+	if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['comments'])) {
+		$error = true;
+	} else {
+		$to = "marveen1520@gmail.com";
+
+		$name = trim($_POST['name']);
+		$email = trim($_POST['email']);
+		$comments = trim($_POST['comments']);
+
+		$subject = "Contact Form";
+
+		$messages = "Name: $name \r\n Email: $email \r\n Comments: $comments";
+		$headers = "From" . $name;
+		$mailsent = mail($to, $subject, $messages, $headers);
+
+		if($mailsent) {
+			$sent = true;
+		}
+	}
+}
+?>
 <!DOCTYPE html PUBLIC "_//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>Marvin A Contact Form</title>
+	<title>Contact Form - Marvin Alcantara</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<link rel="stylesheet" href="c_styles.css">
 </head>
@@ -14,7 +37,7 @@
 		<p class="sent">Thank you. Your email has been sent successfully. We will do our best to reply within 24 hours!</p>
 		<?php } ?>
 		<div id="contactform">
-			<form name="contact" action"<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+			<form name="contact" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 				<label for="name">Name:</label>
 				<input type="text" name="name" />
 				<label for="email">Email:</label>
