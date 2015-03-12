305259fdceedad48c84a5cda15f34ff7776a4c17
maralkan <marveen1520@gmail.com>
2015-03-12 01:38:33 -0700

    Updates to all working files
    index.html, as-am.html, bible-study.html, geo.html, contact.html:
    updated navigation links;
    bible-study.html: updated content;
    mixture.json: updated parameters;
    config.rb: updated parameters;
    new favicons for subfolders;

--- notes/config.rb
+++ notes/config.rb
@@ -2,11 +2,11 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "js"
-fonts_dir = "fonts"
+css_dir = "/css"
+sass_dir = "/scss"
+images_dir = "/images"
+javascripts_dir = "/js"
+fonts_dir = "/fonts"
 
 output_style = :expanded
 
